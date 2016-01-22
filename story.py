4a7b6e335f2e1c7a5cc245fd7f1e7a05d1be4db1
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-01-22 12:45:01 -0500

    Create story.py

---
+++ story.py
@@ -0,0 +1,12 @@
+gender = raw_input('What is your gender?')
+print 'Ah, you are a %s!' % gender
+age = raw_input('So how old are you, exactly?')
+print '%s years old, great!' % age
+name = raw_input('Oh, my! I almost forgot your name! What was it again?')
+print 'Hello, %s!' % name
+print 'So you are a %s, %s years old, and your name is %s?' % (gender, age, name)
+playgameconfirm = raw_input('Yes or No?')
+if playgameconfirm == 'Yes':
+    print 'Well then'
+else:
+    print 'Well then'
