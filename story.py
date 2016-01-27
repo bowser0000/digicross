4e374525520f8abe112f63cf1e76b4e1f9b138d1
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-01-27 14:44:13 -0500

    Update 0.1.1
    Added Functions to call start when No is answered

--- story.py
+++ story.py
@@ -1,12 +1,17 @@
-gender = raw_input('What is your gender?')
-print 'Ah, you are a %s!' % gender
-age = raw_input('So how old are you, exactly?')
-print '%s years old, great!' % age
-name = raw_input('Oh, my! I almost forgot your name! What was it again?')
-print 'Hello, %s!' % name
-print 'So you are a %s, %s years old, and your name is %s?' % (gender, age, name)
-playgameconfirm = raw_input('Yes or No?')
-if playgameconfirm == 'Yes':
-    print 'Well then'
-else:
-    print 'Well then'
+def Custom():
+    gender = raw_input('What is your gender?')
+    print 'Ah, you are a %s!' % gender
+    age = raw_input('So how old are you, exactly?')
+    print '%s years old, great!' % age
+    name = raw_input('Oh, my! I almost forgot your name! What was it again?')
+    print 'Hello, %s!' % name
+    print 'So you are a %s, %s years old, and your name is %s?' % (gender, age, name)
+    def YesNo():
+        playgameconfirm = raw_input('Yes or No?')
+        if playgameconfirm == 'Yes':
+            print 'Well then, let\' get started!'
+        elif playgameconfirm == 'No':
+            Custom()
+        else:
+            print 'Please capitalise the first letter, and use Yes or No!'
+            YesNo()
