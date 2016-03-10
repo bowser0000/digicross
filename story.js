1d6a1ec8739e81ae78822dd949012a83fa0badf9
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-03-10 18:28:10 -0500

    Semicolons and some bug fixes

--- story.js
+++ story.js
@@ -26,101 +26,101 @@ function Custom()
     alert('DigiCross trainer: BEKFAST!');
     alert('Professor: Don\'t fight him! He has no DigiCross!');
     alert('Professor: That was a close one.');
-    alert('I have been waiting to ask you, what should I nickname you?');
+    alert('Professor: I have been waiting to ask you, what should I nickname you?');
     function nickname()
     {
-      var nickname = prompt('Menu: You can set your nickname to your orignal name if you don\'t want one.')
+      var nickname = prompt('Menu: You can set your nickname to your orignal name if you don\'t want one.');
       alert('Professor: So, ' + nickname + ', is it?');
-      var nicknameconfirm = prompt('Menu: Yes or No?').toLowerCase()
+      var nicknameconfirm = prompt('Menu: Yes or No?').toLowerCase();
       if (nicknameconfirm == 'yes')
       {
         alert('Professor: So, ' + nickname + ', shall we go to route 2?');
         alert('We also need you to pick a DigiCross!');
         function choose1()
         {
-          digicross1 = prompt('Will you choose Winrad, Semtec or Ilapod?').toLowerCase()
+          digicross1 = prompt('Will you choose Winrad, Semtec or Ilapod?').toLowerCase();
           function finishpick()
           {
-            alert('Professor: Here, take ' + digicross1 + 'then!');
+            alert('Professor: Here, take ' + digicross1 + ' then!');
             alert('Professor: Take care of him!');
             alert('Professor: I\'ll be on my way! Meet me back home!');
             alert('DigiCross Trainer: Your first battle too? Then let\'s fight!');
             function firstbattlefunc()
             {
-              var firstbattle = prompt('Menu: Would you like to Attack, Special or Flee?').toLowerCase()
+              var firstbattle = prompt('Menu: Would you like to Attack, Special or Flee?').toLowerCase();
             }
-            firstbattlefunc()
+            firstbattlefunc();
             if (firstbattle == 'attack')
             {
               alert('Menu: ' + digicross1 + ' used Rear Kick! Blooma took 7 damage!');
               alert('DigiCross Trainer: You put up a good fight, but is your strength a lie?');
               alert('DigiCross Trainer: Blooms, use tickle... erm... I mean tackle!');
-              alert(WIP)
+              alert(WIP);
               alert(digicross1 + ' took' + 'INSERTRANDOMDAMAGEHERE' + ' damage!');
             }
             else if (firstbattle == 'special')
             {
               alert('Menu: You have no specials yet!');
-              firstbattlefunc()
+              firstbattlefunc();
             }
             else if (firstbattle == 'flee')
             {
               alert('Menu: You can\'t flee from this battle!');
-              firstbattlefunc()
+              firstbattlefunc();
             }
             else
             {
               alert('Menu: Use Attack, Special or Flee!');
-              firstbattlefunc()
+              firstbattlefunc();
             }
           }
           if (digicross1 == 'winrad')
           {
-            digicross1 = 'Winrad'
-            finishpick()
+            digicross1 = 'Winrad';
+            finishpick();
           }
           else if (digicross1 == 'semtec')
           {
-            digicross1 = 'Semtec'
-            finishipick()
+            digicross1 = 'Semtec';
+            finishipick();
           }
-          else if (digicross 1 == 'ilapod')
+          else if (digicross1 == 'ilapod')
           {
-            digicross1 = 'Ilapod'
-            finishpick()
+            digicross1 = 'Ilapod';
+            finishpick();
           }
           else
           {
             alert('Menu: Use Winrad, Semtec or Ilapod!');
-            choose1()
+            choose1();
           }
-          finishpick()
+          finishpick();
         }
-        choose1()
+        choose1();
       }
       else if (nicknameconfirm == 'no')
       {
-        nickname()
+        nickname();
       }
       else
       {
         alert('Use Yes or No!');
-        nickname()
+        nickname();
       }
-      nickname()
+      nickname();
     }
-    
+    nickname();
   }
   else if (playgameconfirm == 'no')
   {
-    Custom()
+    Custom();
   }
   else
   {
     alert('Menu: Use Yes or No!');
-   YesNo()
+   YesNo();
   }
   }
-  YesNo()
+  YesNo();
 }
-Custom()
+Custom();
