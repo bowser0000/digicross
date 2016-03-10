c2c22e9d27f030a7aa7677e3856926ed3955e0d5
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-03-10 17:06:21 -0500

    More lines and quick edits

--- story.js
+++ story.js
@@ -1,16 +1,16 @@
 // If you are a contributor, you get your name here ;)
 // Thanks Loquacious!
 // Work in Progress! Want to help? https://github.com/bowser0000/digicross/
-var WIP = ('To be added!')
+var WIP = ('To be added!');
 
-var gender = prompt('Professer: What is your gender?').toLowerCase()
-alert('Professor: Ah! You are a ' + gender + '!')
-var age = prompt('Professor: So how old are you, exactly?')
-alert('Professor: ' + age + ' years old, great!')
-var name = prompt('Professor: Oh my! I almost forgot your name! What was it again?')
-alert('Professor: Hello ' + name + '!')
-alert('Professor: So you are a ' + gender + ', ' + age + ' years old, and your name is ' + name + '?')
-var playgameconfirm = prompt('Menu: Yes or No?').toLowerCase()
+var gender = prompt('Professer: What is your gender?').toLowerCase();
+alert('Professor: Ah! You are a ' + gender + '!');
+var age = prompt('Professor: So how old are you, exactly?');
+alert('Professor: ' + age + ' years old, great!');
+var name1 = prompt('Professor: Oh my! I almost forgot your name! What was it again?');
+alert('Professor: Hello ' + name + '!');
+alert('Professor: So you are a ' + gender + ', ' + age + ' years old, and your name is ' + name + '?');
+var playgameconfirm = prompt('Menu: Yes or No?').toLowerCase();
 if (playgameconfirm == 'yes')
 {
   
@@ -21,5 +21,5 @@ else if (playgameconfirm == 'no')
 }
 else
 {
-  
+  alert('Menu: Use Yes or No!');
 }
