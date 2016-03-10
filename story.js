6baa2ecc0a25d92261bb0ccfb9e14d0673b5575a
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-03-10 12:20:11 -0500

    Starting story.js
    ur mum

---
+++ story.js
@@ -0,0 +1,6 @@
+// If you are a contributor, you get your name here ;)
+// Thanks Loquacious!
+// Work in Progress! Want to help? https://github.com/bowser0000/digicross/
+var WIP = ('To be added!')
+
+var gender = prompt('Professer: What is your gender?').toLowerCase()
